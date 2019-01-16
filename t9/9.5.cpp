06b17440a4d93bd8df7a6814433117c849d5c743
closer <37889723+closer@users.noreply.github.com>
2019-01-16 23:40:11 +0800

    2019.1.16 update

---
+++ t9/9.5.cpp
@@ -0,0 +1,36 @@
+﻿/*5.从文件TEST中读出字符并写到文件TEST1里，要求均附加错误检查。*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+/*******************************************************************/
+
+/*******************************************************************/
+int main()
+{
+	ifstream tx1("TEST.txt");
+	ofstream tx2("TEST1.txt");
+	if (!tx1) {
+		cout << "打开文件失败！" << endl;
+		return 0;
+	}
+	if (!tx2) {
+		cout << "建立文件失败！" << endl;
+		return 0;
+	}
+	string ch;
+	while (1) {
+		if (!tx1.eof()) {
+			getline(tx1, ch);
+			cout << ch << endl;
+			tx2 << ch << endl;
+		}
+		else {
+			tx1.close();
+			return 0;
+		}
+	}
+	return 0;
+}
+/*******************************************************************/
